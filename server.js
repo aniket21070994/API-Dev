468f43a5a12da1cdcd2c29a0d0e3fbbb4fe09187
aniket21070994 <aniket.mankar001@gmail.com>
2024-04-08 01:16:13 +0530

    api server

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import route from "./routes.js";
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+
+// Import routes from other files (GET, POST etc.)
+app.use("/api",route); 
+
+app.use("/",(req,res)=>{
+    res.send("home pages")
+})
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`)
+})
