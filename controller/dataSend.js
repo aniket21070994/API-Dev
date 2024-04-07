b4412ff5336b560e0245244a291e3e5595388c71
aniket21070994 <aniket.mankar001@gmail.com>
2024-04-08 01:29:23 +0530

    comment

--- controller/dataSend.js
+++ controller/dataSend.js
@@ -2,11 +2,14 @@ import   { data}  from "../model/data.js";
 
 export const sendController=(req,res)=>
 {    
+    //send array data
     res.status(200).send({ success:true,data: data});
             
   
 
 }
+
+  //test pourpose
 export const homeController=(req,res)=>
 {   console.log("home page");
     res.status(200).send("<h1>home page</h1>");
