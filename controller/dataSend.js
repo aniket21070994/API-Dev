38137afe7b395b167240c8c6abe5d4e6718c9958
aniket21070994 <aniket.mankar001@gmail.com>
2024-04-08 01:28:36 +0530

    removed id paramere

--- controller/dataSend.js
+++ controller/dataSend.js
@@ -2,26 +2,8 @@ import   { data}  from "../model/data.js";
 
 export const sendController=(req,res)=>
 {    
-    
-    let id=req.params['id'];
-    console.log();
-    console.log(req.params['id'])
-    if(true)
-      {     console.log("data get");
+    res.status(200).send({ success:true,data: data});
             
-            
-            res.status(200).send(
-            { success:true,
-              data: data[id]
-            }
-        )
-            
-       }
-       else{
-        res.status(200).send({
-            message:"no data found change key "
-        })
-       }
   
 
 }
