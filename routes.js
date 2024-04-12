f7263e64460f63ff8c6e23ea034a7e07d18d9a8a
aniket21070994 <aniket.mankar001@gmail.com>
2024-04-12 22:33:09 +0530

    added comments

--- routes.js
+++ routes.js
@@ -1,21 +1,26 @@
 import express  from 'express'
-import { editController, removeController, sendController,setController } from './controller/dataSend.js';
-const routes=express.Router();
+// Importing the express library to create the server
 
+const routes = express.Router();
+// Creating a new router instance from the express library
 
+// Importing the controller functions for handling API requests
+import { editController, removeController, sendController, setController } from './controller/dataSend.js';
 
-//Api get data route
-//URL=https://api-rxb0.onrender.com/api  provoding index is important while adding,editing data
+// Api get data route
+// URL=https://api-rxb0.onrender.com/api
+// Providing the index is important while adding, editing data
+routes.get('/get', sendController);
+// Get data from API
 
+routes.post("/set", setController);
+// Set data from API
 
-routes.get('/get',sendController); //get data from API
+routes.post('/edit', editController);
+// Edit the data in API
 
-routes.post("/set",setController);// set data from API
-
-routes.post('/edit',editController)//Edit the data in API
-
-routes.delete('/remove',removeController)//remove data from API
-
-
+routes.delete('/remove', removeController);
+// Remove data from API
 
 export default routes;
+// Exporting the router instance for use in other modules
--- server.js
+++ server.js
@@ -1,23 +1,48 @@
+// Import the express library
 import express from "express"
+
+// Import the dotenv library
 import dotenv from "dotenv"
+
+// Import the bodyParser library
 import bodyParser from "body-parser"
+
+// Import the route module
 import route from "./routes.js";
+
+// Import the cors library
 import cors from "cors"
+
+// Import the setdb module
+import setdb from "./model/configdb/db.js";
+
+// Call the config method of the dotenv library
 dotenv.config();
+
+// Create an instance of the express library
 const app = express();
 
+// Call the setdb function
+setdb();
 
+// Use the cors middleware
 app.use(cors());
-app.use(bodyParser.json()); // support json encoded bodies
 
-// Import routes from other files (GET, POST etc.)
+// Use the bodyParser.json() middleware to support json encoded bodies
+app.use(bodyParser.json());
+
+// Use the route module for /api endpoint
 app.use("/api",route); 
 
+// Define a middleware function for the root endpoint
 app.use("/",(req,res)=>{
     res.send("home pages")
 })
+
+// Get the port number from the environment variable or use 3001 as the default
 const port = process.env.PORT || 3001;
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
     console.log(`Server running on ${port}`)
 })
